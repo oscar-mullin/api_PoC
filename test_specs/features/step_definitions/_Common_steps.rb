6c1c6cce69044cd976b1bffd2d84bfbf5c31dcab
TRUEXTEND\walter.ramirez <walter.ramirez@mindjet.com>
2017-02-21 18:12:58 -0400

    Fixed Merge issues

--- test_specs/features/step_definitions/_Common_steps.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../support/API_Objects/community_api'
-
-Then(/^I verify Get response is (.*)$/) do |expectedResponse|
-  fail(ArgumentError.new("Actual response: #{$responseGet.code}, Expected: #{expectedResponse}\n")) unless $responseGet.code == expectedResponse
-end
-
-Then(/^I verify Post response is (.*)$/) do |expectedResponse|
-  fail(ArgumentError.new("Actual response: #{$responsePost.code}, Expected: #{expectedResponse}\n")) unless $responsePost.code == expectedResponse
-end
